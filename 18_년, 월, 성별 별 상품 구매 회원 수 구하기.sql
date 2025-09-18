2d47294c1740e7baf7a5ab68482b02bf1807b690
jinhee kim <lsa68803@naver.com>
2025-09-18 11:01:41 +0900

    feat: [GROUP BY] 년, 월, 성별 별 상품 구매 회원 수 구하기 (LV4)

---
+++ 18_년, 월, 성별 별 상품 구매 회원 수 구하기.sql
@@ -0,0 +1,62 @@
+/*
+ [ GROUP BY ] 년, 월, 성별 별 상품 구매 회원 수 구하기 (Lv4)
+
+ [문제 설명]
+ 다음은 어느 의류 쇼핑몰에 가입한 회원 정보를 담은 USER_INFO 테이블과 온라인 상품 판매 정보를 담은 ONLINE_SALE 테이블 입니다.
+ USER_INFO 테이블은 아래와 같은 구조로 되어있으며 USER_ID, GENDER, AGE, JOINED는 각각 회원 ID, 성별, 나이, 가입일을 나타냅니다.
+
+ GENDER 컬럼은 비어있거나 0 또는 1의 값을 가지며 0인 경우 남자를, 1인 경우는 여자를 나타냅니다.
+
+ ONLINE_SALE 테이블은 아래와 같은 구조로 되어있으며, ONLINE_SALE_ID, USER_ID, PRODUCT_ID, SALES_AMOUNT, SALES_DATE는
+ 각각 온라인 상품 판매 ID, 회원 ID, 상품 ID, 판매량, 판매일을 나타냅니다.
+ 동일한 날짜, 회원 ID, 상품 ID 조합에 대해서는 하나의 판매 데이터만 존재합니다.
+
+ [문제]
+ USER_INFO 테이블과 ONLINE_SALE 테이블에서 년, 월, 성별 별로 상품을 구매한 회원수를 집계하는 SQL 문을 작성해주세요.
+ 결과는 년, 월, 성별을 기준으로 오름차순 정렬해주세요. 이때, 성별 정보가 없는 경우 결과에서 제외해주세요.
+
+ */
+
+-- 조회) 년, 월, 성별 별 상품 구매한 회원수
+SELECT YEAR(o.SALES_DATE) AS YEAR,
+    MONTH(o.SALES_DATE) AS MONTH,
+    u.GENDER,
+-- 주의! 상품 수가 아니라 회원 수 이므로 중복 제거
+--   (한 회원이 상품 여러개 구매했을 수 있으므로)
+    COUNT(DISTINCT o.USER_ID) AS USERS
+FROM USER_INFO u
+    JOIN ONLINE_SALE o
+ON u.USER_ID = o.USER_ID
+-- 조건4) 성별 정보 없는 경우 결과에서 제외
+WHERE GENDER IS NOT NULL
+-- 조건1) 년 별
+GROUP BY YEAR(o.SALES_DATE),
+-- 조건2) 월 별
+    MONTH(o.SALES_DATE),
+-- 조건3) 성별 별
+    u.GENDER
+-- 정렬) 년, 월, 성별 기준 오름차순
+ORDER BY YEAR(o.SALES_DATE),
+    MONTH(o.SALES_DATE),
+    u.GENDER;
+
+/*
+
+      [ 주의1. GROUP BY ]
+
+       - 년, 월, 성별 별 조회 이므로 꼭 GROUP BY에 넣어주기!
+
+       - SELECT 문에 들어가는 집계함수 제외한 컬럼은 꼭 넣기!!
+
+-----------------------------------------------------------------------------
+
+      [ 주의2. DISTINCT ]
+
+       - 년, 월, 성별 별 상품 구매한 회원수 집계이므로
+
+       - 회원 수 (COUNT) 집계 시 중복 제거 필수!
+
+       - 만약, 회원수가 아니라 구매 상품 집계시에는 중복 제거 필요X
+
+ */
+
