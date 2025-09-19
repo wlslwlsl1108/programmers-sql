4113a8d517d54a1ffc2f0d426c2d7e9283c27f03
jinhee kim <lsa68803@naver.com>
2025-09-19 11:21:23 +0900

    feat: [OTHERS] 우유와 요거트가 담긴 장바구니 (Lv4)

---
+++ OTHERS/02_우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,28 @@
+/*
+ [ OTHERS ] 우유와 요거트가 담긴 장바구니 (Lv4)
+ (Summer/Winter Coding(2019))
+
+ [문제 설명]
+ CART_PRODUCTS 테이블은 장바구니에 담긴 상품 정보를 담은 테이블입니다.
+ CART_PRODUCTS 테이블의 구조는 다음과 같으며, ID, CART_ID, NAME, PRICE는
+ 각각 테이블의 아이디, 장바구니의 아이디, 상품 종류, 가격을 나타냅니다.
+
+ [문제]
+ 데이터 분석 팀에서는 우유(Milk)와 요거트(Yogurt)를 동시에 구입한 장바구니가 있는지 알아보려 합니다.
+ 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성해주세요.
+ 이때 결과는 장바구니의 아이디 순으로 나와야 합니다.
+
+ */
+
+-- 조회  ) 장바구니 ID
+SELECT CART_ID
+FROM CART_PRODUCTS
+-- 조건  ) 우유와 요거트를 동시에 구입한 장바구니
+WHERE NAME IN ('Milk', 'Yogurt')
+GROUP BY CART_ID
+-- DISTINCT : Milk, Milk 이런식으로 동일물품 구매할 경우 중복 카운트 방지
+HAVING COUNT(DISTINCT NAME) = 2
+-- 정렬  ) 장바구니 ID 기준으로 오름차순
+ORDER BY CART_ID;
+
+
